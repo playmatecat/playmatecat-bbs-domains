a81207da2eac82ee4f2f73d80d2e51ebf730d231
Isa <Isa@Isa-PC>
2015-06-21 17:09:01 +0800

    1

--- playmatecat-domain-sys_bbs/src/main/java/com/playmatecat/domains/sysBBS/dto/RoleDTO.java
+++ playmatecat-domain-sys_bbs/src/main/java/com/playmatecat/domains/sysBBS/dto/RoleDTO.java
@@ -3,25 +3,5 @@ package com.playmatecat.domains.sysBBS.dto;
 import com.playmatecat.domains.sysBBS.entity.Role;
 
 public class RoleDTO extends Role{
-    private int pageSize;
-    
-    private int pageNo;
 
-    public int getPageSize() {
-        return pageSize;
-    }
-
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    public int getPageNo() {
-        return pageNo;
-    }
-
-    public void setPageNo(int pageNo) {
-        this.pageNo = pageNo;
-    }
-    
-    
 }
